deb652ac4c6402ffe21c985399cd5fa2845f0b8a
Vegadrone <spenaantonino@gmail.com>
2022-11-18 10:25:55 +0100

    complete ex

--- src/org/java60/bank/Main.java
+++ src/org/java60/bank/Main.java
@@ -5,5 +5,14 @@ public class Main {
 		Conto contoProva = new Conto(52201, "Pippo Franco" );
 		
 		System.out.println(contoProva);
+		
+		contoProva.addMoney(1000);
+		System.out.println("Il tuo nuovo saldo è: " + contoProva.getSaldo() + " " + "€");
+		
+		contoProva.getMoney(150);
+		System.out.println("Il tuo nuovo saldo è: " + contoProva.getSaldo() + " " + "€");
+		
+		contoProva.getMoney(950);
+		System.out.println("Il tuo nuovo saldo è: " + contoProva.getSaldo() + " " + "€");
 	}
 }
