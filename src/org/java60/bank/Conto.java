9a8b0018705836e5ff7348efdfc4474f751169eb
Vegadrone <spenaantonino@gmail.com>
2022-11-18 10:17:45 +0100

    complete Conto class and start testing

--- src/org/java60/bank/Conto.java
+++ src/org/java60/bank/Conto.java
@@ -1,5 +1,60 @@
 package org.java60.bank;
 
 public class Conto {
-
+	
+	//Variabili ambiente
+	
+	private int numConto;
+	private int saldo;
+	private String nomeProprietario;
+	
+	//Costruttore
+	
+	public Conto (int numConto, String nomeProprietario) {
+		saldo = 0;
+		this.numConto = numConto;
+		this.nomeProprietario = nomeProprietario;
+	}
+	
+	//Setter and Getter
+	
+	public int getSaldo() {
+		return saldo;
+	}
+	
+	public int getNumConto() {
+		return numConto;
+	}
+	
+	public String getNomeProprietario() {
+		return nomeProprietario;
+	}
+	
+	public void setNomeProprietario(String nomeProprietario) {
+		this.nomeProprietario = nomeProprietario;
+	}
+	
+	//Metodi
+	
+	public void addMoney(int money) {
+		saldo += money;
+	}
+	
+	public void getMoney( int money) {
+		if((saldo- money) < 0) {
+			System.out.println("Il tuo saldo non può essere meno di 0");
+		} else {
+			saldo -= money;
+		}
+	}
+	
+	@Override
+	public String toString() {
+	
+		return "nomeProprietario: " + getNomeProprietario() + "\nNumero Conto: " + getNumConto() + "\nSaldo: " + getSaldo() + " " + "€";
+	}
+	
+	
+	
+	
 }
--- src/org/java60/bank/Main.java
+++ src/org/java60/bank/Main.java
@@ -1,5 +1,9 @@
 package org.java60.bank;
 
 public class Main {
-
+	public static void main(String[] args) {
+		Conto contoProva = new Conto(52201, "Pippo Franco" );
+		
+		System.out.println(contoProva);
+	}
 }
